e935cdccb1d20b9a2eec263af14653c3030f7d6b
ChuDucManh <89825108+manhchuduc@users.noreply.github.com>
2023-03-03 01:01:12 +0700

    update biggest

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.onkeydown = function (e) {
+	if (e.keyCode == 123) {
+		return false;
+	}
+
+	// disable I key
+	if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+		return false;
+	}
+
+	// disable J key
+	if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+		return false;
+	}
+
+	// disable U key
+	if (e.ctrlKey && e.keyCode == 85) {
+		return false;
+	}
+};
