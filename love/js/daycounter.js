348ddb35dbefd6d842848ca910509ceb690cb46d
ChuDucManh <89825108+manhchuduc@users.noreply.github.com>
2023-03-28 13:25:06 +0700

    Remake all

--- love/js/daycounter.js
+++ love/js/daycounter.js
@@ -1,25 +1,25 @@
 var ngayyeu = "11/02/2022 13:30:00";
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-document.onkeydown = function (e) {
-	if (e.keyCode == 123) {
-		return false;
-	}
+// document.addEventListener("contextmenu", (event) => event.preventDefault());
+// document.onkeydown = function (e) {
+// 	if (e.keyCode == 123) {
+// 		return false;
+// 	}
 
-	// disable I key
-	if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-		return false;
-	}
+// 	// disable I key
+// 	if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+// 		return false;
+// 	}
 
-	// disable J key
-	if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-		return false;
-	}
+// 	// disable J key
+// 	if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+// 		return false;
+// 	}
 
-	// disable U key
-	if (e.ctrlKey && e.keyCode == 85) {
-		return false;
-	}
-};
+// 	// disable U key
+// 	if (e.ctrlKey && e.keyCode == 85) {
+// 		return false;
+// 	}
+// };
 const get_day_of_time = (d1, d2) => {
 	let ms1 = d1.getTime();
 	let ms2 = d2.getTime();
@@ -64,95 +64,112 @@ const get_sec = (d1, d2) => {
 		return ans;
 	}
 };
+document.getElementById("circle").addEventListener("click", change);
+document.getElementById("date").addEventListener("click", change);
 function change() {
-	let ele = document.getElementById("day");
-	let check = ele.classList.contains("date");
-	if (check) {
-		ele.classList.remove("date");
-		date();
+	let circle = document.getElementById("circle");
+	let date = document.getElementById("date");
+
+	checker = circle.classList.contains("hide");
+	if (checker) {
+		date.classList.add("hide");
+		circle.classList.remove("hide");
 	} else {
-		ele.classList.add("date");
-		let when = new Date(ngayyeu);
-		let todate = new Date();
-		let time = get_day_of_time(when, todate);
-		time -= 1;
-
-		let year = time / 365;
-		year = parseInt(year);
-		let month = time / 30;
-		month = parseInt(month);
-		let day = time % 30;
-		let week = day / 7;
-		week = parseInt(week);
-		day = day - week * 7;
-
-		if (year === 0) {
-			year = "";
-		} else {
-			year = year + 'năm <i class="fa-solid fa-heart fa-bounce heart"></i> ';
-		}
-		if (month === 0) {
-			month = "";
-		} else {
-			month =
-				month + 'tháng <i class="fa-solid fa-heart fa-bounce heart"></i> ';
-		}
-		if (week === 0) {
-			week = "";
-		} else {
-			week = week + 'tuần <i class="fa-solid fa-heart fa-bounce heart"></i> ';
-		}
-		if (day === 0) {
-			day = "";
-		} else {
-			day = day + "ngày";
-		}
-		ele.innerHTML = year + month + week + day;
+		circle.classList.add("hide");
+		date.classList.remove("hide");
 	}
 }
+function calander() {
+	let when = new Date(ngayyeu);
+	let todate = new Date();
+	let time = get_day_of_time(when, todate);
+	time--;
+
+	let yearele = document.getElementById("year");
+	monthele = document.getElementById("month");
+	weekele = document.getElementById("week");
+	dayele = document.getElementById("day");
+
+	let year = time / 365;
+	year = parseInt(year);
+	let month = time / 30;
+	month = parseInt(month);
+	let day = time % 30;
+	let week = day / 7;
+	week = parseInt(week);
+	day = day - week * 7;
+
+	year = year + "<br>Năm</br>";
+	month = month + "<br>Tháng</br>";
+	week = week + "<br>Tuần</br>";
+	day = day + "<br>Ngày</br>";
+
+	dayele.innerHTML = day;
+	yearele.innerHTML = year;
+	monthele.innerHTML = month;
+	weekele.innerHTML = week;
+}
+calander();
+dayruner();
 function date() {
 	let when = new Date(ngayyeu);
 	let todate = new Date();
 	let time = get_day_of_time(when, todate);
-	time -= 1;
+	time--;
 
-	const daycount = document.getElementsByClassName("counter")[0];
+	const daycount = document.getElementById("daycount");
 	const start = document.getElementById("start");
 
 	let whenyear = when.getFullYear();
 	let whenmonth = when.getMonth() + 1;
 	let whenday = when.getDate();
+	if (whenday - 10 < 0) {
+		whenday = "0" + whenday;
+	}
+	if (whenmonth - 10 < 0) {
+		whenmonth = "0" + whenmonth;
+	}
 
-	start.innerHTML = whenyear + "-" + whenmonth + "-" + whenday;
-	daycount.innerHTML =
-		time + ' <i class="fa-solid fa-heart fa-bounce heart"></i> Ngày';
-}
-function time() {
-	let when = new Date(ngayyeu);
-	let todate = new Date();
+	start.innerHTML = whenday + "-" + whenmonth + "-" + whenyear;
+
 	let hour = get_hour(when, todate);
 	let min = get_min(when, todate);
 	let sec = get_sec(when, todate);
 	if (hour == 0 && min == 0 && sec == 0) {
-		time += 1;
+		time++;
+		calander();
+		dayruner();
 	}
-	if (hour == 0 && min >= todate.getHours()) {
+	if (hour == 0 && min >= todate.getMinutes()) {
 		hour = parseInt(hour);
 		hour += 23;
 	}
 	const clock = document.getElementById("clock");
-	clock.innerHTML = hour + " : " + min + " : " + sec;
-	setTimeout(time, 1000);
+	clock.innerHTML =
+		'<div class="clock">' +
+		hour +
+		'</div>:<div class="clock">' +
+		min +
+		'</div>:<div class="clock">' +
+		sec +
+		"</div>";
+
+	setTimeout(date, 1000);
 }
-time();
-function repeat() {
-	let day = document.getElementById("day");
-	let check = day.classList.contains("date");
-	if (check) {
-		setTimeout(repeat, 1000);
-	} else {
-		date();
-	}
-	setTimeout(repeat, 1000);
+date();
+
+function dayruner() {
+	let when = new Date(ngayyeu);
+	let todate = new Date();
+	let time = get_day_of_time(when, todate);
+	time--;
+
+	var day = 0;
+	let days = setInterval(() => {
+		day++;
+		daycount.innerHTML = day + " Ngày";
+		if (day == time) {
+			clearInterval(days);
+		}
+	}, 1);
 }
-repeat();
