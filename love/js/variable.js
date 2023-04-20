1688e5df74edd703d29b969e992a498a9e902aba
ChuDucManh <89825108+manhchuduc@users.noreply.github.com>
2023-04-20 23:59:52 +0700

    Update variable.js

--- love/js/variable.js
+++ love/js/variable.js
@@ -1,6 +1,6 @@
 var ngayyeu = "03/08/2023 00:00:00"; // MM/DD/YYYY h/m/s VD:"30/07/2023 13:30:00"
 
-var ngaysinh1 = "03/20/2008";
+var ngaysinh1 = "09/20/2008";
 var gioitinh1 = "nam";
 var ngaysinh2 = "07/30/2009";
 var gioitinh2 = "nu";
