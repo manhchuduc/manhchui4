28ac95a57bfff3ca1943120540bd68734d6b8772
ChuDucManh <89825108+manhchuduc@users.noreply.github.com>
2023-09-15 22:17:25 +0700

    new tools v0

---
+++ tools/js/a.js
@@ -0,0 +1,21 @@
+num = false;
+spe = false;
+
+function randompass(length) {
+	var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	var number = "0123456789987654321";
+	if (num) {
+		chars += number;
+	}
+	var special = "!@#$%^&*()-_=+,./;'[]<>?:";
+	if (spe) {
+		chars += special;
+	}
+	var pass = "";
+	for (var i = 0; i < length + 1; i++) {
+		vitri = Math.floor(Math.random() * chars.length + 1);
+		pass += cut = chars.slice(vitri, vitri + 1);
+	}
+	return pass;
+}
+console.log(randompass(10));
--- tools/js/scripts.js
+++ tools/js/scripts.js
@@ -158,3 +158,25 @@ function close() {
 	document.querySelector("#get").removeEventListener("click", getCountDays);
 	blur.innerHTML = "";
 }
+
+// !Random pass
+num = false;
+spe = false;
+
+function randompass(length) {
+	var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	var number = "0123456789987654321";
+	if (num) {
+		chars += number;
+	}
+	var special = "!@#$%^&*()-_=+,./;[]<>?:";
+	if (spe) {
+		chars += special;
+	}
+	var pass = "";
+	for (var i = 0; i < length + 1; i++) {
+		vitri = Math.floor(Math.random() * chars.length + 1);
+		pass += cut = chars.slice(vitri, vitri + 1);
+	}
+	return pass;
+}
